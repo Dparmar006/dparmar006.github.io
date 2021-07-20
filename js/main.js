dcfa0e13cd345ebec68aaa00285b88bd23afd86d
Dixit Parmar <Dparmar006@users.noreply.github.com>
2021-07-20 13:39:59 +0530

    navbar height

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ function navToggle() {
   let mobNavButton = document.getElementById("mobile-nav-toggle");
   let mobNav = document.getElementById("mobile-nav");
   mobNavButton.addEventListener("click", () => {
-    mobNavButton.innerHTML == `<i class="fas fa-bars"></i>`;
-    if (mobNavButton.innerHTML == `<i class="fas fa-bars"></i>`) {
+    // mobNavButton.innerHTML == `<i class="fas fa-bars"></i>`;
+    if (mobNavButton.innerHTML == `<i class="fas fa-times"></i>`) {
       // mobNavButton.innerHTML = "";
-      mobNavButton.innerHTML = `<i class="fas fa-times"></i>`;
-      mobNav.style.top = "0%";
-    } else {
+      mobNav.style.height = "0px";
       mobNavButton.innerHTML = `<i class="fas fa-bars"></i>`;
-      mobNav.style.top = "-100%";
+    } else {
+      mobNavButton.innerHTML = `<i class="fas fa-times"></i>`;
+      mobNav.style.height = "fit-content";
     }
   });
 }

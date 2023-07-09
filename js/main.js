f9cd85beb560eb78f6e1db5cd4f72c1b33fa7a48
Dixit Parmar <55636593+Dparmar006@users.noreply.github.com>
2023-07-09 11:34:46 +0000

    Added UI changes, Expierieance calculated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-window.onload = navToggle;
+window.onload = () => {
+  navToggle();
+  const startDate = "2021-05-01"
+  const duration = calculateDuration(startDate);
+  document.getElementById("exp-val").innerHTML = `( Total: ${duration.years} Years, ${duration.months} months ) `;
+}
 
 function navToggle() {
   let mobNavButton = document.getElementById("mobile-nav-toggle");
@@ -14,4 +19,24 @@ function navToggle() {
       mobNav.style.height = "fit-content";
     }
   });
+}
+
+
+function calculateDuration(startDate) {
+  let today = new Date();
+  let start = new Date(startDate);
+
+  let years = today.getFullYear() - start.getFullYear();
+  let months = today.getMonth() - start.getMonth();
+  let days = today.getDate() - start.getDate();
+
+  if (months < 0 || (months === 0 && days < 0)) {
+    years--;
+    months += 12;
+  }
+
+  return {
+    years: years,
+    months: months
+  };
 }
